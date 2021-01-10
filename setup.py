5b8b205d56a9efdb424c6aa63b1ff6561c074eed
ddonng <ddonng@qq.com>
2021-01-11 00:46:30 +0800

    reserve picture

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 ﻿from setuptools import setup
 
 setup(name='docx-mailmerge-hxd',
-      version='0.7.1',
+      version='0.7.2',
       description='Performs a Mail Merge on docx (Microsoft Office Word) files',
       long_description=open('README.rst','r',encoding='UTF-8').read(),
       classifiers=[
